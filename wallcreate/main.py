89ba3058b607c17e7a5f6fb3ba622fb9f344a5a3
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-11 19:39:48 -0500

    Base Pillow code

--- wallcreate/main.py
+++ wallcreate/main.py
@@ -1,7 +1,11 @@
-from PIL import
+from PIL import Image, ImageDraw, ImageFilter
+from pathlib import Path
 
 def main():
-    pass
+    base_image = Image.new("RGBA",(1920, 1200), color='white')
+    logo = Image.open('assets/distros/NixOS/nixos.svg')
 
+    base_image.paste(logo)
+    base_image.save('img.png')
 if __name__ == "__main__":
     main()
